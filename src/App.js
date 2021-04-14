906a9e4d881ab89259d9e7c709097c9f11f47ce2
Kim Young Jo <yoyounn88@gmail.com>
2021-04-14 19:31:25 +0900

    JSON 관련 실패하긴함

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import StartBtn from './components/StartBtn'
 import Meso from './Meso.mp3'
 import Exp15 from './15min.mp3'
 import Exp30 from './30min.mp3'
-import { useState } from 'react'
+import Event from './components/Event'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [start, setStart] = useState(false)
+  const [event, setEvent] = useState([])
 
   const looting = 110
   const exp15 = 900
@@ -16,12 +18,34 @@ function App() {
     setStart(!start)
   }
 
+  useEffect(() => {
+    const getEvent = async () => {
+      const eventFromServer = await fetchEvent()
+      setEvent(eventFromServer)
+    }
+    getEvent()
+  }, [])
+
+  const fetchEvent = async () => {
+    const res = await fetch('http://localhost:5000')
+    const data = await res.json()
+
+    return data
+  }
+
+  const onClick = () => {
+    console.log(event)
+  }
+
   return (
     <div className="App">
       <StartBtn onClick={startOnClick} text={start === false ? '시작' : '리셋'} /> <br />
       {start && <Count second={looting} src={Meso} />}일시정지<br />회수
       {start && <Count second={exp15} src={Exp15} />}일시정지<br />15분 경쿠
-      {start && <Count second={exp30} src={Exp30} />}일시정지<br />30분 경쿠
+      {start && <Count second={exp30} src={Exp30} />}일시정지<br />30분 경쿠<br />
+      <button onClick={onClick}>
+        JSON
+      </button>
     </div>
   );
 }
